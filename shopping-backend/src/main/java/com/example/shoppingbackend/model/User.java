2833a7fc9acde662fd70202c9fb84087369d6b50
darshan-kumar-19012 <79315464+darshan-kumar-19012@users.noreply.github.com>
2022-08-17 17:53:52 +0530

    backend v0.1

---
+++ shopping-backend/src/main/java/com/example/shoppingbackend/model/User.java
@@ -0,0 +1,92 @@
+package com.example.shoppingbackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_no")
+    private String phoneNo;
+
+    public User() {
+    }
+
+    public User(String userName, String email, String password, String address, String phoneNo) {
+        super();
+        this.userName = userName;
+        this.email = email;
+        this.address = address;
+        this.password = password;
+        this.phoneNo = phoneNo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
+
