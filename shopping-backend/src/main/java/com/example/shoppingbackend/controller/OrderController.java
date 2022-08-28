37a44caf4a03dd0159317ade88a205d66b875c96
darshan-kumar-19012 <79315464+darshan-kumar-19012@users.noreply.github.com>
2022-08-28 20:35:58 +0530

    added search in order controller

--- shopping-backend/src/main/java/com/example/shoppingbackend/controller/OrderController.java
+++ shopping-backend/src/main/java/com/example/shoppingbackend/controller/OrderController.java
@@ -7,7 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.shoppingbackend.repository.OrderRepository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -69,4 +70,10 @@ public class OrderController {
         response.put("deleted", Boolean.TRUE);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/orders/search/{keyword}")
+    public Page<Order> getAllBooks(Pageable pageable,@PathVariable("keyword") String keyword) {
+        return orderRepository.findAll(pageable,keyword);
+    }
+
 }
